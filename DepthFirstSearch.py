1c1466d924314558a50067cc08cb6bd56a8aaf4f
Fanuel Fiseha Gebru <145048534+cheazer@users.noreply.github.com>
2025-09-25 10:40:41 +0100

    Add files via upload

---
+++ DepthFirstSearch.py
@@ -0,0 +1,20 @@
+def DFS(Graph, Vertex):
+    visited = set()
+    stack = [Vertex]
+    Order = []
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            Order.append(node)
+            for neighbor in Graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+    return Order
+
+raph1 = {'A': ['B','C'],
+         'B': ['F'],
+         'C': ['B'],
+         'F': []}
+
+print(BFS(raph1, 'A'))
